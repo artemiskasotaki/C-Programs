ce02c13e62ddaf74a72ce36a737f77ce760732b0
Artemis <52401191+artemiskasotaki@users.noreply.github.com>
2021-01-10 19:19:35 +0200

    Update Days Converter.c

--- Days Converter/Days Converter.c
+++ Days Converter/Days Converter.c
@@ -8,9 +8,9 @@ int main(void){
 	weeks=(days%365)/7;
 	days=days-((years*365)+(weeks*7));
 	
-	printf("%d:",years);
-	printf("%d:",weeks);
-	printf("%d:",days);
+	printf("Years:%d \n",years);
+	printf("Weeks:%d \n",weeks);
+	printf("Days:%d \n",days);
 	
 	return 0;
 }
